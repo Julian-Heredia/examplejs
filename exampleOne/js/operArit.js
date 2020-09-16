ec80bf13938f15532020e805a5da59120efe1278
Julian-Heredia <julian00hs@gmail.com>
2020-09-15 19:15:34 -0500

    agregado el metodo para calcular el area de un circulo

--- exampleOne/js/operArit.js
+++ exampleOne/js/operArit.js
@@ -24,15 +24,23 @@ document.getElementById("resulOperacion").innerHTML = resultado;
 }
 
 
-// function calcular_Area_Triangulo()
-// {
-// //Recibir los datos
-// let BBase = parseInt(document.getElementById("Base").value);
-// let AAltura = parseInt(document.getElementById("Altura").value);
+function Calcular_Area_Triangulo()
+{
+//Recibir los datos
+let BBase = parseInt(document.getElementById("Base").value);
+let AAltura = parseInt(document.getElementById("Altura").value);
 
-// //Procesar los datos
-// let resultado =  parseInt(BBase * AAltura) / 2;
+//Procesar los datos
+let resultado =  parseInt(BBase * AAltura) / 2;
 
-// //Entregar los resultados
-// document.getElementById("resulOperacionT").innerHTML = resultado;
-// }
+//Entregar los resultados
+document.getElementById("resulOperacionT").innerHTML = resultado;
+}
+
+function Calcular_Area_Circulo()
+{
+var pi = 3.141595654
+let Radio = parseInt(document.getElementById("radio").value);
+let resultado = parseFloat(pi * Radio * Radio)
+document.getElementById("resulOperacionC").innerHTML = resultado;
+}
